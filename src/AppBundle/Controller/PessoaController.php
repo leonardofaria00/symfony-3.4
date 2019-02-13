04f7814f9574c2e53b4c6752b568208c6e4f2efd
leonardofaria00 <leonardofaria00@gmail.com>
2019-02-13 12:06:14 -0200

    melhorando view

--- src/AppBundle/Controller/PessoaController.php
+++ src/AppBundle/Controller/PessoaController.php
@@ -15,36 +15,44 @@ use AppBundle\Entity\Pessoa;
 class PessoaController extends Controller {
 
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="api_pessoa_indexAction")
      */
     public function indexAction(Request $request) {
         return new JsonResponse(['msg' => 'PessoaController']);
     }
 
     /**
-     * @Route("/lista", name="api_pessoa_lista")
+     * @Route("/lista", name="api_pessoa_listaAction")
      */
-    public function index() {
+    public function listaAction() {
         $pessoa = $this->getDoctrine()->getRepository('AppBundle:Pessoa')->findAll();
         $pessoa = $this->get('jms_serializer')->serialize($pessoa, 'json');
 
-        $response = new Response($pessoa, 200);
-        $response->headers->set('Content-Type', 'application/json');
-
-        return $response;
-        //return $this->render('pessoa/index.html.twig', ['nome'=>'Leonardo Faria dos Santos']);
+        $response = new Response($pessoa);
+//        $response->headers->set('Content-Type', 'application/json');
+//        return $response;
+        return $this->render('pessoa/index.html.twig', ['pessoas' => $response]);
+//        return $this->renderView('pessoa/index.html.twig', ['pessoas' => $response]);
+//        return $this->renderJson('pessoa/index.html.twig', ['pessoas' => $response]);
     }
 
     /**
-     * @Route("/lista/{id}", name="api_pessoa_show")
+     * @Route("/lista/{id}", name="api_pessoa_showAction")
      */
-    public function show(Pessoa $pessoa) {
+    public function showAction(Pessoa $pessoa) {
         $pessoa = $this->get('jms_serializer')->serialize($pessoa, 'json');
 
         $response = new Response($pessoa, 200);
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
+    }
+
+    /**
+     * @Route("/add", name="api_pessoa_addAction")
+     */
+    public function addAction() {
+        return $this->render('pessoa/adiciona.html.twig');
     }
 
 }
