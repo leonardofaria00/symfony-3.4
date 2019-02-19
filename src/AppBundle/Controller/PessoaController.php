466e43cd102dc0b3401f95a26357b78710b1a8ec
leonardofaria00 <leonardofaria00@gmail.com>
2019-02-19 11:50:12 -0300

    subindo index e view

---
+++ src/AppBundle/Controller/PessoaController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Pessoa;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Pessoa controller.
+ *
+ * @Route("/pessoa")
+ */
+class PessoaController extends Controller {
+
+    /**
+     * Lists all pessoa entities.
+     *
+     * @Route("/", name="_index")
+     * @Method("GET")
+     */
+    public function indexAction() {
+        return $this->render('pessoa/index.html.twig');
+    }
+
+    /**
+     * Lists all pessoa entities.
+     *
+     * @Route("/view", name="_view")
+     * @Method("GET")
+     */
+    public function listaAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $pessoas = $em->getRepository('AppBundle:Pessoa')->findAll();
+
+        return $this->render('pessoa/view.html.twig', array('pessoas' => $pessoas,));
+    }
+
+    /**
+     * Creates a new pessoa entity.
+     *
+     * @Route("/new", name="_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request) {
+        $pessoa = new Pessoa();
+        $form = $this->createForm('AppBundle\Form\PessoaType', $pessoa);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($pessoa);
+            $em->flush();
+
+            return $this->redirectToRoute('_show', array('id' => $pessoa->getId()));
+        }
+
+        return $this->render('pessoa/new.html.twig', array('pessoa' => $pessoa, 'form' => $form->createView(),));
+    }
+
+    /**
+     * Finds and displays a pessoa entity.
+     *
+     * @Route("/{id}", name="_show")
+     * @Method("GET")
+     */
+    public function showAction(Pessoa $pessoa) {
+        $deleteForm = $this->createDeleteForm($pessoa);
+
+        return $this->render('pessoa/show.html.twig', array('pessoa' => $pessoa, 'delete_form' => $deleteForm->createView(),));
+    }
+
+    /**
+     * Displays a form to edit an existing pessoa entity.
+     *
+     * @Route("/{id}/edit", name="_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Pessoa $pessoa) {
+        $deleteForm = $this->createDeleteForm($pessoa);
+        $editForm = $this->createForm('AppBundle\Form\PessoaType', $pessoa);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('_edit', array('id' => $pessoa->getId()));
+        }
+
+        return $this->render('pessoa/edit.html.twig', array('pessoa' => $pessoa, 'edit_form' => $editForm->createView(), 'delete_form' => $deleteForm->createView(),));
+    }
+
+    /**
+     * Deletes a pessoa entity.
+     *
+     * @Route("/{id}", name="_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Pessoa $pessoa) {
+        $form = $this->createDeleteForm($pessoa);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($pessoa);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('_index');
+    }
+
+    /**
+     * Creates a form to delete a pessoa entity.
+     *
+     * @param Pessoa $pessoa The pessoa entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Pessoa $pessoa) {
+        return $this->createFormBuilder()->setAction($this->generateUrl('_delete', array('id' => $pessoa->getId())))->setMethod('DELETE')->getForm();
+    }
+
+}
---
+++ src/AppBundle/Form/PessoaType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PessoaType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nome')->add('identidade')->add('login')->add('senha');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Pessoa'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_pessoa';
+    }
+
+
+}
